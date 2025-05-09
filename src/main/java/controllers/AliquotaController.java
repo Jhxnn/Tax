106b5e06333b5fa78e6435dc956d710729340c34
Hilixx <jhonalizan@gmail.com>
2025-05-09 19:05:16 -0300

    feature: controllers

--- src/main/java/controllers/AliquotaController.java
+++ src/main/java/controllers/AliquotaController.java
@@ -1,4 +1,27 @@
 package controllers;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import services.AliquotaService;
+
+
+@Path("/aliquota")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class AliquotaController {
+
+    @Inject
+    AliquotaService aliquotaService;
+
+    @GET
+    public Response findAll(){
+        return Response.ok(aliquotaService.findAll()).build();
+    }
+
 }
--- src/main/java/controllers/ImpostoController.java
+++ src/main/java/controllers/ImpostoController.java
@@ -1,4 +1,28 @@
 package controllers;
 
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import services.ImpostoService;
+
+@Path("/imposto")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ImpostoController {
+
+
+    @Inject
+    ImpostoService impostoService;
+
+    @GET
+    public Response findAll(){
+
+        return  Response.ok(impostoService.findAll()).build();
+    }
+
+
 }
