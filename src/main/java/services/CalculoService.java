c4df3e6b1a2747f12130c3d9d6c370e6d71f7cda
Hilixx <jhonalizan@gmail.com>
2025-05-11 12:30:51 -0300

    feature: calculos de imposto

--- src/main/java/services/CalculoService.java
+++ src/main/java/services/CalculoService.java
@@ -1,5 +1,8 @@
 package services;
 
+import dtos.CalculoICMSDto;
+import dtos.CalculoIPIDto;
+import dtos.CalculoISSDto;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
@@ -13,7 +16,19 @@ public class CalculoService {
     ImpostoService impostoService;
 
 
-    public
+    public double calcularICMS(CalculoICMSDto calculoICMSDto, double valorProduto){
+        double percentual = aliquotaService.retornarPercentualICSM(calculoICMSDto) / 100;
+        return  valorProduto * percentual;
+
+    }
+    public double calcularIPI(CalculoIPIDto calculoIPIDto, double valoProduto){
+        double percentual = aliquotaService.retornarPercentualIPI(calculoIPIDto) / 100;
+        return valoProduto * percentual;
+    }
+    public double calcularISS(CalculoISSDto calculoISSDto, double valorServico){
+        double percentual = aliquotaService.retornarPercentualISS(calculoISSDto) / 100;
+        return valorServico * percentual;
+    }
 
 
 
