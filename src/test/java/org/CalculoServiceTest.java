c72ca40b558fdf3e4a1f3d87f9cbc2977f918a83
Hilixx <jhonalizan@gmail.com>
2025-05-12 14:25:48 -0300

    tests: finally

--- src/test/java/org/CalculoServiceTest.java
+++ src/test/java/org/CalculoServiceTest.java
@@ -11,8 +11,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import services.AliquotaService;
 import services.CalculoService;
 
-import static io.restassured.RestAssured.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CalculoServiceTest {
@@ -27,22 +27,24 @@ class CalculoServiceTest {
     @Test
     void testCalcularICMS() {
         CalculoICMSDto dto = new CalculoICMSDto("SP", "SC", "VENDA", "eletronico", 1000);
+        when(aliquotaService.retornarPercentualICSM(dto)).thenReturn(17.0);
         double percentual = aliquotaService.retornarPercentualICSM(dto) / 100;
-        assertEquals(170.0, dto.valorProduto() * percentual, 0.01);  // Tolerância de 0.01
+        assertEquals(170.0, dto.valorProduto() * percentual, 0.01);
     }
 
     @Test
     void testCalcularIPI() {
         CalculoIPIDto dto = new CalculoIPIDto("VENDA", "eletronico", 1000);
-
+        when(aliquotaService.retornarPercentualIPI(dto)).thenReturn(15.0);
         double percentual = aliquotaService.retornarPercentualIPI(dto) / 100;
         assertEquals(150.0, dto.valorProduto() * percentual, 0.01);
+
     }
 
     @Test
     void testCalcularISS() {
         CalculoISSDto dto = new CalculoISSDto("Joinville", "SERVICO", "tecnologia", 1000);
-
+        when(aliquotaService.retornarPercentualISS(dto)).thenReturn(2.5);
         double percentual = aliquotaService.retornarPercentualISS(dto) / 100;
         assertEquals(25.0, dto.valorServico() * percentual, 0.01);
     }
